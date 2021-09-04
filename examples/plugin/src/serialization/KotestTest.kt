007f598f3dc906d5dc18095a3038d685e98c98b9
Jesse Schalken <me@jesseschalken.com>
2021-09-04 17:42:04 +1000

    try to add kotest to examples/plugin/src/serialization

---
+++ examples/plugin/src/serialization/KotestTest.kt
@@ -0,0 +1,11 @@
+package kotesttest
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.runner.junit4.KotestTestRunner
+import org.junit.runner.RunWith
+
+@RunWith(KotestTestRunner::class)
+class HelloWorldTest : FreeSpec() {
+  init {
+  }
+}
